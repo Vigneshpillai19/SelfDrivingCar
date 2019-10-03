4fe1b55bebff5806a179ea477a1564f61ed212cf
Harshita <h2010singh@gmail.com>
2019-10-03 15:35:15 +0530

    Final Code

--- drive_control/drive_control.ino
+++ drive_control/drive_control.ino
@@ -2,11 +2,28 @@ const int EnableL = 5;
 const int HighL = 6;     //Left Side Motors
 const int LowL = 7;
 
-const int EnableL = 10;
+const int EnableR = 10;
 const int HighR = 8;    //Right Side Motors
 const int LowR = 9;
 
+// Arduino and RaspberryPi Connecting pins
+//const int D0 = 0; // Rpi pin 21 LSB
+//const int D1 = 1; // Rpi pin 22
+//const int D2 = 2; // Rpi pin 23
+//const int D3 = 3; // Rpi pin 24 MSB
 
+//int a,b,c,d,data=0;
+// Reading data received from Rpi
+
+/*void Data(){
+  a = digitalRead(D0);
+  b = digitalRead(D1);
+  c = digitalRead(D2);
+  d = digitalRead(D3);
+
+  data = 8*d+4*c+2*b+a;
+}*/
+ 
 void setup() {
   // put your setup code here, to run once:
   // Enabling the pins for receiving the output
@@ -17,8 +34,13 @@ void setup() {
   pinMode(EnableR,OUTPUT);
   pinMode(HighR,OUTPUT);
   pinMode(LowR,OUTPUT);
+ 
+  /*pinMode(D0,INPUT_PULLUP);
+  pinMode(D1,INPUT_PULLUP);
+  pinMode(D2,INPUT_PULLUP);
+  pinMode(D3,INPUT_PULLUP);*/  
   // end method
-};
+}
 
 void Forward(){
   digitalWrite(HighL,LOW);  //Code may change for our motor from low to high
@@ -103,8 +125,24 @@ void Left3(){
   analogWrite(EnableR,255);
 }
 
+void Stop(){
+  digitalWrite(HighL,HIGH);
+  digitalWrite(LowL,LOW);   //Code may change for our motor from low to high 
+  analogWrite(EnableL,0); //and vice versa
+
+  digitalWrite(HighR,HIGH); //Code may change for our motor from low to high
+  digitalWrite(LowR,LOW);   // and vice versa
+  analogWrite(EnableR,0);
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
   Forward();
+  //Left1();
+  //Right1();
+  //Backward();
+  //Data(); // data from Rpi
+
+  // similarly for different scenarios, write commands
+
 }

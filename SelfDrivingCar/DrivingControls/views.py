ea46e969886184ea1c3e070d63ee8fef8dbcbff9
Harshita <h2010singh@gmail.com>
2019-11-05 21:04:41 +0530

    final tcp

--- SelfDrivingCar/DrivingControls/views.py
+++ SelfDrivingCar/DrivingControls/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render, redirect
+# from urllib.request import urlopen
 
 # Create your views here.
 import socket
 # conn = ""
+
+# Thingspeak API
+# APIkey = "2O0L57XKSJE58ENR"
+# baseURL = "https://api.thingspeak.com/update?api_key=2O0L57XKSJE58ENR"
+
 def index(request):
     return render(request,"index.html")
 
-localIP   = "192.168.1.7"
+localIP   = "192.168.1.105"
 localPort = 20001
 
-#print("hello")
+print("hello")
 
 bufferSize = 1024
 
@@ -28,16 +34,18 @@ TCPServerSocket.listen(5)
 conn, addr = TCPServerSocket.accept()
 
 def stream(request):
-    bytesToSend = "1".encode()
-    conn.sendall(bytesToSend)
+    # bytesToSend = "1".encode()
+    # conn.sendall(bytesToSend)
     send()
     return render(request, "stream.html")
 
 def send():
     bytesToSend = "1".encode()
     conn.sendall(bytesToSend)
+    # conn = urlopen(baseURL + '&field1=1')
 
 def stop(request):
     bytesToSend = "0".encode()
     conn.sendall(bytesToSend)
+    # conn = urlopen(baseURL + '&field1=0')
     return redirect("/")
